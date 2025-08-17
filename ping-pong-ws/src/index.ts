7ba185340e2315fada1d8f9bcad5fcb43f8d56e1
Shikhar Singh <146508977+spywar18@users.noreply.github.com>
2025-08-17 17:36:42 +0530

    Update index.ts

--- ping-pong-ws/src/index.ts
+++ ping-pong-ws/src/index.ts
@@ -1,9 +1,7 @@
 // Import the WebSocketServer class from the "ws" library
 import { WebSocketServer } from "ws";
-
 // Create a new WebSocket server instance on port 8000
 const wss = new WebSocketServer({ port: 8000 });
-
 // Add an event listener for new client connections
 wss.on("connection", function(socket) {
     // Add an event listener for messages received from the connected client
